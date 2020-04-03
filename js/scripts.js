427bca9de9ef43876684d1aaeb9122d395b4872f
me <me@me.me.com>
2020-04-03 13:12:47 -0700

    create jquery calls to access all input types and display them

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,31 @@
 
 $(document).ready(function(){
+  
   $("#formOne").submit(function(e){
+    e.preventDefault();
     var number = $("#favoritenumber").val();
     var favoriteColor = $("#favoritecolor").val();
+    
     var favoriteFood = $("#favoritefood").val();
     var favoriteSubject =$("input:radio[name=favoritesubject]:checked").val();
     var birthdate = $("#dateofbirth").val();
 
+
+
+    $(".favoritenumber").text(number);
+    $(".favoritefood").text(favoriteFood);
+    $(".favoritesubject").text(favoriteSubject);
+    $(".favoritecolor").css({
+      "backgroundColor": favoriteColor,
+      "height":"25px",
+      "width":"25px"
+    });
+    $(".birthdate").text(birthdate);
+
     $(".survey-results").show();
-})
+    debugger;
+  });
+
+});
+
 
